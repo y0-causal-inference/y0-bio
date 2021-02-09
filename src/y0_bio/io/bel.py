833f53bab0a7983531fd2a846f15487eb1edbc22
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-02-09 21:38:37 +0100

    Update tests and implementation

--- src/y0_bio/io/bel.py
+++ src/y0_bio/io/bel.py
@@ -2,21 +2,105 @@
 
 """Import BEL to Ananke."""
 
+from typing import Optional
+
+import pybel.constants as pc
 from ananke.graphs import ADMG
 from pybel import BELGraph
 
+from y0.graph import NxMixedGraph
 
-def bel_to_ananke(graph: BELGraph) -> ADMG:
-    """Convert a BEL Graph to an Ananke ADMG.
+__all__ = [
+    'bel_to_nxmg',
+    'bel_to_admg',
+    'bel_to_causaleffect',
+]
+
+CORRELATIVE_RELATIONS = pc.CORRELATIVE_RELATIONS | {pc.CORRELATION}
+VALID = {'di', 'bi', 'skip'}
+
+
+def bel_to_nxmg(
+    graph: BELGraph,
+    include_associations: bool = False,
+    indirect_handler: Optional[str] = None,
+) -> NxMixedGraph:
+    """Convert a BEL Graph to a y0 networkx mixed graph.
 
     Rules:
 
     - Directly increases, directly decreases, and directly regulates all become directed edges
-    - Optional: increases, decreases, and regulate can either become directed or not
+    - Optional: increases, decreases, and regulates become bidirected edges because there might
+      be some confounders in the middle
     - Positive correlation, negative correlation, and correlation become bidirected edges
-    - Optional: association becomes bidirected edges
+    - Association edges are excluded by default, could be optionally included
 
     :param graph: A BEL graph
+    :param include_associations: Should :data:`pybel.constants.ASSOCIATION` relationships be included as bidirected
+        edges in the graph? Defaults to false.
+    :param indirect_handler: How should indirected edges be handled? If 'bi', adds as bidirected edges. Elif 'di',
+        adds as bidirected edges. If 'skip', do not include. If None, defaults to 'bi'.
+    :return: A y0 networkx mixed graph
+    """
+    rv = NxMixedGraph()
+    if indirect_handler is None:
+        indirect_handler = 'bi'
+    if indirect_handler not in VALID:
+        raise ValueError(f'invalid indirect edge handler: {indirect_handler}. Should be in {VALID}')
+    for u, v, d in graph.edges(data=True):
+        if d[pc.RELATION] in CORRELATIVE_RELATIONS:
+            rv.add_undirected_edge(u.name, v.name)
+        elif include_associations and d[pc.RELATION] == pc.ASSOCIATION:
+            rv.add_undirected_edge(u.name, v.name)
+        elif d[pc.RELATION] in pc.DIRECT_CAUSAL_RELATIONS:
+            rv.add_directed_edge(u.name, v.name)
+        elif d[pc.RELATION] in pc.INDIRECT_CAUSAL_RELATIONS:
+            if indirect_handler == 'bi':
+                rv.add_undirected_edge(u.name, v.name)
+            elif indirect_handler == 'di':
+                rv.add_directed_edge(u.name, v.name)
+    return rv
+
+
+def bel_to_admg(
+    graph: BELGraph,
+    include_associations: bool = False,
+    indirect_handler: Optional[str] = None,
+) -> ADMG:
+    """Convert a BEL Graph to an Ananke ADMG.
+
+    :param graph: A BEL graph
+    :param include_associations: Should :data:`pybel.constants.ASSOCIATION` relationships be included as bidirected
+        edges in the graph? Defaults to false.
+    :param indirect_handler: How should indirected edges be handled? If 'bi', adds as bidirected edges. Elif 'di',
+        adds as bidirected edges. If 'skip', do not include. If None, defaults to 'bi'.
     :return: An Ananke ADMG
     """
-    raise NotImplementedError
+    nxmg = bel_to_nxmg(
+        graph,
+        include_associations=include_associations,
+        indirect_handler=indirect_handler,
+    )
+    return nxmg.to_admg()
+
+
+def bel_to_causaleffect(
+    graph: BELGraph,
+    include_associations: bool = False,
+    indirect_handler: Optional[str] = None,
+) -> ADMG:
+    """Convert a BEL Graph to a CausalEffect R graph object.
+
+    :param graph: A BEL graph
+    :param include_associations: Should :data:`pybel.constants.ASSOCIATION` relationships be included as bidirected
+        edges in the graph? Defaults to false.
+    :param indirect_handler: How should indirected edges be handled? If 'bi', adds as bidirected edges. Elif 'di',
+        adds as bidirected edges. If 'skip', do not include. If None, defaults to 'bi'.
+    :return: A CausalEffect R graph object
+    """
+    nxmg = bel_to_nxmg(
+        graph,
+        include_associations=include_associations,
+        indirect_handler=indirect_handler,
+    )
+    return nxmg.to_causaleffect()
--- tests/io/test_bel.py
+++ tests/io/test_bel.py
@@ -4,14 +4,17 @@
 
 import unittest
 
-from ananke.graphs import ADMG
 from pybel import BELGraph
 from pybel.dsl import Protein
 
-from y0_bio.io.bel import bel_to_ananke
+from y0.graph import NxMixedGraph
+from y0_bio.io.bel import bel_to_nxmg
 
+TEST_CITATION = '1234'
+TEST_EVIDENCE = 'ev'
 A = Protein(namespace='hgnc', name='A')
 B = Protein(namespace='hgnc', name='B')
+C = Protein(namespace='hgnc', name='C')
 
 
 class TestBELImport(unittest.TestCase):
@@ -19,19 +22,21 @@ class TestBELImport(unittest.TestCase):
 
     def admg_equal(self, expected, actual, msg=None):
         """Check that two ADMGs are equivalent."""
-        self.assertIsInstance(expected, ADMG)
-        self.assertIsInstance(actual, ADMG)
-        self.assertEqual(expected, actual, msg=msg)
+        self.assertIsInstance(expected, NxMixedGraph)
+        self.assertIsInstance(actual, NxMixedGraph)
+        self.assertEqual(set(expected.directed.edges()), set(actual.directed.edges()), msg=msg)
+        self.assertEqual(set(expected.undirected.edges()), set(actual.undirected.edges()), msg=msg)
 
     def test_import_causal(self):
         """Check a directly increases."""
         bel_graph = BELGraph()
-        bel_graph.add_directly_increases(A, B, citation='', evidence='')
-        actual = bel_to_ananke(bel_graph)
-        expected = ADMG(
-            vertices=['A', 'B'],
-            di_edges=[
+        bel_graph.add_directly_increases(A, B, citation=TEST_CITATION, evidence=TEST_EVIDENCE)
+        bel_graph.add_directly_decreases(B, C, citation=TEST_CITATION, evidence=TEST_EVIDENCE)
+        actual = bel_to_nxmg(bel_graph)
+        expected = NxMixedGraph.from_edges(
+            directed=[
                 ('A', 'B'),
+                ('B', 'C'),
             ],
         )
         self.admg_equal(expected, actual)
